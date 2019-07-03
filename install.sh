13bd80c3c40704d7401c784ef19e62e2e7c5d823
root <root@debian-testserver>
2019-07-03 19:32:28 +1000

    add sed

--- install.sh
+++ install.sh
@@ -3,8 +3,21 @@
 
 apt install zabbix-agent -y
 
-cp -fr ./etc/zabbix/* /etc/zabbix/
+cp -fr ./scripts /etc/zabbix/
+cp -fr ./zabbix_agentd.conf.d /etc/zabbix/
 
 /usr/bin/crontab -l > crontabnew
 cat ./crontab/crontab >> crontabnew
 /usr/bin/crontab crontabnew
+
+
+
+sed -i 's/.*Server=127.0.0.1.*/Server=zabbix.game-forest.com/' /etc/zabbix/zabbix_agentd.conf 
+sed -i 's/.*ServerActive=127.0.0.1.*/ServerActive=zabbix.game-forest.com/' /etc/zabbix/zabbix_agentd.conf 
+sed -i 's/.*Hostname=Zabbix server.*/HostnameItem=system.hostname/' /etc/zabbix/zabbix_agentd.conf 
+
+
+#sed -i 's/.*ШАБЛОН.*/ЗАМЕНЯЮЩАЯ_СТРОКА/' ФАЙЛ
+
+
+/etc/init.d/zabbix-agent restart
